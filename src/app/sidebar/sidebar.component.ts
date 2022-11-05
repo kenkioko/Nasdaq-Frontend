7c023a89adae2a4160e15901ca454cde5cc3e4e3
Kioko Kennedy <kmkioko@gmail.com>
2022-11-05 07:18:01 +0300

    Company Listing Events

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AppComponent } from '../app.component';
 import { DataProvider } from '../utility/data-provider';
+import $ from 'jquery';
 
 @Component({
   selector: 'app-sidebar',
@@ -10,8 +11,9 @@ import { DataProvider } from '../utility/data-provider';
 export class SidebarComponent implements OnInit {
   title: string;
   listing: any;
+  // selected: any;
 
-  constructor() { 
+  constructor() {
     // app title
     let app = new AppComponent;
     this.title = app.title;
@@ -21,10 +23,29 @@ export class SidebarComponent implements OnInit {
     // company listings
     let provider = new DataProvider()
     provider.companyList((results: any) => {
-      this.listing = results;
-
-      console.log('listing', this.listing);
+      this.listing = results.hasOwnProperty('data')
+        ? results['data']
+        : results;
     });
+  }
+
+  onHover(event: Event, toggle: boolean) {
+    // // if selected prevent
+    // if (toggle === true) {
+    //   event.preventDefault();
+    // }
+    
+    if (event.target instanceof Element) {
+      let elem = $(event.target);
+      
+      elem.toggleClass('active', toggle)
+        .attr('aria-current', toggle.toString());
+    }
+  }
+
+  onClick(event: Object) {
+    console.log(event);
+
   }
 
   ngOnInit(): void {
